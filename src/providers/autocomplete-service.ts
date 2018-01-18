9e01580c95912b7dd242766ef2f33ccf35f53afd
jvitus <jeanvitus.albertini@gmail.com>
2018-01-18 15:19:59 +0100

    Change request we didn't send RANG = 'SSES' now

--- src/providers/autocomplete-service.ts
+++ src/providers/autocomplete-service.ts
@@ -62,7 +62,7 @@ export class CompleteTaxaService {
               })
                 .then((db: SQLiteObject) => { 
                   console.log('open SQL');
-                  db.executeSql('SELECT CD_NOM AS taxref_id, NOM_VERN AS label, NOM_VERN AS vernaculaire, LB_NOM AS latin, RANG AS Rang FROM '+tableName+' WHERE NOM_NORMALISE LIKE "%'+itemNormalise+'%" ORDER BY NOM_VERN ASC', {})
+                  db.executeSql('SELECT CD_NOM AS taxref_id, NOM_VERN AS label, NOM_VERN AS vernaculaire, LB_NOM AS latin, RANG AS Rang FROM '+tableName+' WHERE NOM_NORMALISE LIKE "%'+itemNormalise+'%" AND RANG NOT LIKE "SSES"  ORDER BY NOM_VERN ASC', {})
                     .then((res) => {
                       db.close();
                       var data = []
